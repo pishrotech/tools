eb08b08f46812b16b75fe97b436e7c880d19b847
mahyar <mohammadmahdisherafat79@gmail.com>
2021-12-26 12:20:52 +0330

    ....

---
+++ scripts/getSelectedValueJquary.js
@@ -0,0 +1,5 @@
+$('select').on('change', function () {
+    //ways to retrieve selected option and text outside handler
+    console.log('Changed option value ' + this.value);
+    console.log('Changed option text ' + $(this).find('option').filter(':selected').text());
+  });
