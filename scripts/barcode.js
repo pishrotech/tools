84cd7932e4b1853d661137cb9ea7dea129a41672
mahyar <mohammadmahdisherafat79@gmail.com>
2021-12-27 11:21:20 +0330

    test

---
+++ scripts/barcode.js
@@ -0,0 +1,29 @@
+on_scanner() // init function
+
+function on_scanner() {
+    let is_event = false; // for check just one event declaration
+    let input = document.getElementById("scanner"); // input
+    input.addEventListener("focus", function () {
+        if (!is_event) {
+            is_event = true;
+            input.addEventListener("keypress", function (e) {
+                setTimeout(function () {
+                    if (e.keyCode == 13) {
+                        scanner(input.value); // use value as you need
+                        input.select();
+                    }
+                }, 500)
+            })
+        }
+    });
+    document.addEventListener("keypress", function (e) {
+        if (e.target.tagName !== "INPUT") {
+            input.focus();
+        }
+    });
+}
+
+function scanner(value) {
+    if (value == '') return;
+    console.log(value)
+}
