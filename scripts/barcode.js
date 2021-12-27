8f06a5e881f952f55a06ffab886f74034949e1c8
mahyar <mohammadmahdisherafat79@gmail.com>
2021-12-27 12:06:48 +0330

    edit barcode scaner

--- scripts/barcode.js
+++ scripts/barcode.js
@@ -2,18 +2,17 @@ on_scanner() // init function
 
 function on_scanner() {
     let is_event = false; // for check just one event declaration
-    let input = document.getElementById("scanner"); // input
+    let input = document.getElementById("barcode"); // input
     input.addEventListener("focus", function () {
         if (!is_event) {
             is_event = true;
             input.addEventListener("keypress", function (e) {
-                setTimeout(function () {
-                    if (e.keyCode == 13) {
-                        scanner(input.value); // use value as you need
-                        input.select();
-                    }
-                }, 500)
-            })
+                if (e.keyCode == 13) {
+                    scanner(input.value); // use value as you need
+                    input.select();
+                }
+            }
+            )
         }
     });
     document.addEventListener("keypress", function (e) {
